86d619e6997da2a14b2fe300fb7ea0dadd639873
waleedahmad7970 <waleedahmad7970@gmail.com>
2026-08-13 23:52:46 +0500

    log

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+console.log('API URL:', import.meta.env.VITE_API_URL);
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
   headers: {
